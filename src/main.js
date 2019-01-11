290f79ae9bfb39202fef8d67850e84203cef34a5
Simon <simon.j.goring@gmail.com>
2019-01-10 16:15:08 -0800

    Had to add routing for the site so that we could deal with ORCID cookies.

--- src/main.js
+++ src/main.js
@@ -1,14 +1,39 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router';
 import BootstrapVue from 'bootstrap-vue'
+import VueCookies from 'vue-cookies'
 
 import App from './App.vue'
 
 Vue.use(BootstrapVue);
+Vue.use(require('vue-cookies'))
+Vue.use(VueCookies)
+
 Vue.config.productionTip = false
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      component: App,
+      name: 'root'
+    },
+    {
+      path: "/:dsid/",
+      component: App,
+      props: true
+    }
+  ]
+})
+
+Vue.use(VueRouter)
+
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  router: router,
+  render: h => h('router-view')
+})
