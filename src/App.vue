879c4d4fb5563f6a2c6ffe94e14bbf3761d65102
Simon <simon.j.goring@gmail.com>
2020-03-20 18:35:12 -0700

    Added the cookie notice element.

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,29 @@
 <template>
   <div id="app" class="mainpage">
-    <app-header></app-header>
-    <app-start></app-start>
+    <app-header v-show="started"></app-header>
+    <app-element v-show="started"></app-element>
+
+    <app-graph @startup="getStarted" v-show="!started"></app-graph>
+    <cookies />
     <!--<app-insertform></app-insertform>-->
   </div>
 </template>
 
+<styles>
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 2s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+</styles>
+
 <script>
   //import InsertForm from './components/insertform.vue'
   import header from './components/header.vue'
-  import startForm from './components/simple_search.vue'
+  import throughGraph from './components/firstgraph.vue'
+  import elements from './components/head_buttons.vue'
+  import cookieLaw from './components/cookie_note.vue'
 
   import './assets/containers.css'
   // import './assets/text.css'
@@ -17,10 +31,22 @@
 
   export default {
     name: 'app',
+    data() {
+      return {
+        started: false
+      }
+    },
     components: {
-      'app-start': startForm,
+      'app-element' : elements,
+      'app-graph': throughGraph,
       //'app-insertform': InsertForm,
-      'app-header': header
+      'app-header': header,
+      'cookies': cookieLaw
+    },
+    methods: {
+      getStarted(e) {
+        this.started = e;
+      }
     }
   }
 </script>
