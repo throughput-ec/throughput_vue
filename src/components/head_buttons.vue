df749cac659677ea3ce5658bd5122d65e2e8c356
Simon <simon.j.goring@gmail.com>
2020-03-20 18:33:56 -0700

    Starting the header buttons. We want to add in buttons for Metrics, Search and an "About" page.

---
+++ src/components/head_buttons.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <b-container class="bv-example-row">
+      <b-button-toolbar aria-label="Website navigation bar">
+        <b-button-group>
+          <b-button class='mx-2'>Metrics</b-button>
+        </b-button-group>
+        <b-button-group>
+          <b-button>Metrics</b-button>
+          <b-button>Search</b-button>
+          <b-button>Metrics</b-button>
+        </b-button-group>
+      </b-button-toolbar>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import '../assets/containers.css'
+
+export default {
+  name: 'opener',
+  data() {
+    return {
+      loading: false,
+      options: ['Status', 'Searches', 'Something']
+    }
+  }
+}
+</script>
