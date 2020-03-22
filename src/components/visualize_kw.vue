4ea7dcd31c35b4359acedfcf513e90295b20def0
Simon <simon.j.goring@gmail.com>
2020-03-22 13:18:38 -0700

    Stub for keyword visualization

---
+++ src/components/visualize_kw.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <b-card title="Keyword Search">
+    </b-card>
+  </div>
+</template>
+<script>
+export default {
+  name: 'keywordSearch',
+  data() {
+    return {
+      status: true,
+      keywords: []
+      }
+    },
+  }
+</script>
