2a160b6fb007cf1a1b0d7544d99a26e8db2c0ed5
Simon <simon.j.goring@gmail.com>
2020-03-24 16:42:16 -0700

    Keyword search bar modified.  Some more work needed to have top keywords updating.

--- src/components/keyword_search.vue
+++ src/components/keyword_search.vue
@@ -1,7 +1,18 @@
 <template>
   <div>
     <b-card title="Keyword Search">
-      <b-form-input v-model="kwText" placeholder="Keyword"></b-form-input>
+      <b-input-group prepend="Keyword" class="mt-3">
+        <b-form-input v-model="kwText"></b-form-input>
+        <b-input-group-append>
+          <b-button variant="primary">Search</b-button>
+          <b-button v-b-modal.modal-1>Info</b-button>
+          <b-modal id="modal-1">
+            <strong>Keyword search</strong>: Begin typing keywords.
+            As you limit the potential set of keywords a set of options will appear in blue below.
+            Click on a term to add it to your search set.  Click on it again to remove it from the search set.
+          </b-modal>
+        </b-input-group-append>
+      </b-input-group>
       <div v-if="keyresults.length > 0">
         Selected:
         <span v-for="(item, index) in keyresults" v-bind:key="index">
@@ -11,7 +22,7 @@
         </span>
       </div>
       <div v-if="somekw.length < 30">
-        <span v-for="item in somekw" v-bind:key="item.keyword">
+        <span v-for="(item, index) in somekw" v-bind:key="index">
           <span style="margin-right: 2px;margin-bottom: 2px;font-size:14px;">
             <b-badge v-on:click="addkw(item.keyword)" variant="primary">{{ item.keyword }}</b-badge>
           </span>
@@ -20,8 +31,8 @@
       <div v-if="somekw.length === 0 & !kwText === ''">
         No keywords available.
       </div>
-      <div v-if="somekw.length>0">
-        There are {{somekw.length}} available keywords.
+      <div v-if="somekw.length > 1">
+        The top {{ Math.min(30,somekw.length) }} of {{somekw.length}} available keywords are shown.
       </div>
 
       <div style="padding:10px;">
@@ -43,7 +54,7 @@
         keyword: '',
         keyresults: [],
         allkw: [],
-        somekw: [],
+        somekw: [{keyword:'', items:''}],
         kwText: ''
         }
       },
