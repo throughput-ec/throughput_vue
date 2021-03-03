85e971b5a1a626cf6976f901ce541a89fa6d3cdf
Simon Goring <simon.j.goring@gmail.com>
2021-03-02 23:03:30 -0800

    Fixing Citations, dropping citations for repos for now.

--- src/components/repo_lister.vue
+++ src/components/repo_lister.vue
@@ -119,7 +119,7 @@
                     .map(x => x.id)
                     .join(",");
 
-                fetch(`https://${process.env.VUE_APP_URLPATH}/api/citations?ids=${self.ids}`)
+                fetch(`${process.env.VUE_APP_URLPATH}/api/citations?ids=${self.ids}`)
                     .then(function(response) {
                         return response.json();
                     })
