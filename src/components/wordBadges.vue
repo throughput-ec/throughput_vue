6d881ee8a24762499a644c35a83ad1de7a75b26f
SimonGoring <goring@wisc.edu>
2021-06-02 14:23:06 -0700

    Updating wordbadges to simplify template
    This change allows us to pass in a smaller & simpler object that can be parsed from the allkw object in the parent template.

--- src/components/wordBadges.vue
+++ src/components/wordBadges.vue
@@ -1,14 +1,18 @@
 <!-- This is called from the keyword search. -->
-<!-- The goal is to have it all run smoothly, passing in a list of keywords 
-     and returning a keyword name if the kw is to be dropped. -->
+<!-- The goal is to have it all run smoothly, passing in a short list of keywords 
+     whether positive or negative, and returning a keyword name if the kw is to 
+     be dropped. 
+     This would allow us to keep the full KW list in the parent component, and only
+     have a short list of keywords in this component. -->
+<!--  -->
 
 <template>
   <div>
     <b-container style="padding-top: 5px">
-      <h3>{{ this.title }}</h3>
       <!-- Text to explain the available search. -->
       <div v-if="(badgein.length > 0) & (show === true)">
         <small>
+          <h3>{{ this.title }}</h3>
           The top {{ Math.min(30, badgein.length) }} of
           {{ badgein.length }} available keywords are shown. Click on one of the
           words to add it to your set.<br /><strong
@@ -17,7 +21,8 @@
           >
         </small>
       </div>
-      <div v-if="show === false">
+      <div v-if="(badgein.length > 0) & (show === false)">
+        <h3>{{ this.title }}</h3>
         <small>
           Click a term below to remove it from the selected terms.
         </small>
@@ -26,11 +31,7 @@
       <!-- Badges for selected terms -->
       <div v-if="badgein.length > 0" class="keyword-container">
         <div v-for="index in Math.min(30, badgein.length)" :key="index">
-          <div
-            v-if="badgein[index - 1].show === show"
-            class="keyword-badge"
-            v-bind:class="{ 'red-badge': !show }"
-          >
+          <div class="keyword-badge" v-bind:class="{ 'red-badge': !show }">
             <!-- Clicking one of these span elements turns the show parameter
                  on or off.  That happens in "keyword_search"-->
             <span v-on:click="$emit('termOut', badgein[index - 1].term)">{{
